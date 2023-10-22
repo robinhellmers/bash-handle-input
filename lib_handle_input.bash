e1896ae78f92e7fed1fb2c99c698c0d48f98b671
Robin Hellmers <robin@hellmers.se>
2023-10-22 17:55:16 +0200

    Add handle_input_arrays_dynamically()

--- lib_handle_input.bash
+++ lib_handle_input.bash
@@ -39,3 +39,44 @@ init_lib
 #####################
 ### Library start ###
 #####################
+
+# Used for handling arrays as function parameters
+# Creates dynamic arrays from the input
+# 1: Dynamic array name prefix e.g. 'input_arr'
+#    Creates dynamic arrays 'input_arr1', 'input_arr2', ...
+# 2: Length of array e.g. "${#arr[@]}"
+# 3: Array content e.g. "${arr[@]}"
+# 4: Length of the next array
+# 5: Content of the next array
+# 6: ...
+handle_input_arrays_dynamically()
+{
+    local dynamic_array_prefix="$1"; shift
+    local array_suffix=1
+
+    local is_number_regex='^[0-9]+$'
+
+    while (( $# )) ; do
+        local num_array_elements=$1; shift
+
+        if ! [[ "$num_array_elements" =~ $is_number_regex ]]
+        then
+            echo "Given number of array elements is not a number: $num_array_elements"
+            exit 1
+        fi
+        
+        eval "$dynamic_array_prefix$array_suffix=()";
+        while (( num_array_elements-- > 0 )) 
+        do
+
+            if ((num_array_elements == 0)) && ! [[ "${1+nonexistent}" ]]
+            then
+                # Last element is not set
+                echo "Given array contains less elements than the explicit array size given."
+                exit 1
+            fi
+            eval "$dynamic_array_prefix$array_suffix+=(\"\$1\")"; shift
+        done
+        ((array_suffix++))
+    done
+}
